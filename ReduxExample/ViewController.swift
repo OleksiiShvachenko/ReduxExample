9235f01be96366ecf4c6cd61c31dfa61541f97f8
Oleksii Shvachenko <saol@ciklum.com>
2017-08-03 13:28:03 +0300

    add xcodeproj.

---
+++ ReduxExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReduxExample
+//
+//  Created by Oleksii Shvachenko on 03.08.17.
+//  Copyright © 2017 Ciklum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
